6729cec4d5e0b2c88b04d911d9ce870d887d653d
Malithi2021 <abayadeeramr.20@itfac.mrt.ac.lk>
2022-08-01 10:15:02 +0530

    test

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebApplication2.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        bool flag = false;
+        public ActionResult Index()
+        {
+            ViewBag.flagvalue = flag;
+            return View();
+        }
+        [HttpPost]
+     
+        public ActionResult About(FormCollection frm)
+        {
+            flag = true;
+            ViewBag.flagvalue = flag;
+            ViewBag.Name = frm["name"].ToString();
+            ViewBag.Addr = frm["addr"].ToString();
+            return View();
+        }
+
+     
+
+
+    }
+}
